29774b57a189a4834f0112d88f7ac03f3563240e
Peter Martin <petem103072@gmail.com>
2026-01-17 13:39:39 -0600

    changed default window size

--- desktopApp/src/desktopMain/kotlin/com/financeapp/desktop/Main.kt
+++ desktopApp/src/desktopMain/kotlin/com/financeapp/desktop/Main.kt
@@ -1,7 +1,9 @@
 package com.financeapp.desktop
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import com.financeapp.App
 import com.financeapp.di.appModules
 import org.koin.core.context.startKoin
@@ -12,9 +14,14 @@ fun main() {
     }
 
     application {
+        val windowState = rememberWindowState(
+            width = 1400.dp,
+            height = 900.dp
+        )
         Window(
             onCloseRequest = ::exitApplication,
-            title = "FinanceApp"
+            title = "FinanceApp",
+            state = windowState
             // Note: Add icon parameter when app-icon.png is placed in resources/icons/
             // icon = painterResource("icons/app-icon.png")
         ) {
