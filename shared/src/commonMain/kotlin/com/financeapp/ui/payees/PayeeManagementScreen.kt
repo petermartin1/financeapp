ef904053329f544e5688882ad4e1118df338c141
Peter Martin <petem103072@gmail.com>
2026-02-07 15:13:35 -0600

    fix: add missing clickable import in PayeeManagementScreen
    Pre-existing compile error - missing import for clickable modifier.

--- shared/src/commonMain/kotlin/com/financeapp/ui/payees/PayeeManagementScreen.kt
+++ shared/src/commonMain/kotlin/com/financeapp/ui/payees/PayeeManagementScreen.kt
@@ -1,6 +1,7 @@
 package com.financeapp.ui.payees
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
